0585191c625dcce942081c8e9cf714fb1175fa90
ceguster <cguster@gmail.com>
2014-12-03 12:29:42 -0500

    Current_user, makes sure you're logged in, added first name last name attributes, devise done except styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ #HOST METHODS
+ has_many :events, :foreign_key => 'host_id'
+
+ #GUEST METHODS
+ has_many :contributions, :through => :events, :foreign_key => 'guest_id'
+
+
+ def full_name
+  "#{self.first_name} #{self.last_name}"
+ end
+
 end
+
