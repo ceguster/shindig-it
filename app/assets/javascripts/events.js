6b8a078c0c0be38828ff5b08a6b35a0e42bc515b
ceguster <cguster@gmail.com>
2014-12-07 15:08:22 -0500

    claim unclaim works

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,18 +3,40 @@ $(document).ready(function(){
 
   $('#wrapper').on('click', '.unclaimed', function(e){
     e.preventDefault();
+    // e.stopPropagation();
     var eventId = $(this).parent().data('id');
     var contributionId = $(this).parent().attr('id');
     var liEl = $(this).parent();
     var username = $(this).parents('ul').attr('id');
-
     $.ajax ({
 
       url: '/events/' + eventId + '/contributions/' + contributionId ,
       method: 'patch',
       success: function(){
         $('#unclaimed-' + contributionId).remove();
-        $(liEl).append('claimed by ' + username + ' - <div class="claimed" id="claimed-' + contributionId + '">Unclaim</div>');
+        $(liEl).append('<div id="claim-name-' + contributionId + '">claimed by ' + username + ' -</div><div class="claimed" id="claimed-' + contributionId + '">Unclaim</div>');
+      }
+
+    });
+    
+  });
+
+  $('#wrapper').on('click', '.claimed', function(e){
+    e.preventDefault();
+    // e.stopPropagation();
+    var eventId = $(this).parent().data('id');
+    var contributionId = $(this).parent().attr('id');
+    var liEl = $(this).parent();
+    var claimName = $(this).siblings('#claim-name-' + contributionId)
+
+    $.ajax ({
+
+      url: '/events/' + eventId + '/contributions/' + contributionId ,
+      method: 'patch',
+      success: function(){
+        $('#claimed-' + contributionId).remove();
+        $(claimName).remove();
+        $(liEl).append('<div class="unclaimed" id="unclaimed-' + contributionId + '">Claim</div>');
       }
 
     });
