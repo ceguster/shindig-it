e0970c649311d55bdf73989311ffe610e540ebe3
ceguster <cguster@gmail.com>
2014-12-07 18:03:21 -0500

    changed dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,11 +21,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    user_name: 'flatiron.shindigit@gmail.com',
-    password: '1586amiright',
-    authentication: 'plain',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    authentication: :plain ,
     enable_starttls_auto: true
   }
 
