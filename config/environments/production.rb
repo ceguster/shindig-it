95cc3815f73c35e723116cfde271738747dc84c9
ceguster <cguster@gmail.com>
2014-12-07 17:30:16 -0500

    changed config prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
     port: 587,
     user_name: 'flatiron.shindigit@gmail.com',
     password: ENV['email_password'],
-    domain: 'gmail.com',
+    domain: 'shindigit.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true
   }
