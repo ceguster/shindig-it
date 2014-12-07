f70826bdd2b1fae4642263d0da029d3171dd3076
ceguster <cguster@gmail.com>
2014-12-07 18:42:31 -0500

    fuck email fuck

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,13 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
+    @url = 'shindigit.herokuapp.com'
     mail(to: user.email, subject:"Welcome to ShindigIt!")
   end
 
   def invitation_email(user)
     @user = user
+    @url = 'shindigit.herokuapp.com'
     mail(to: user.email, subject: "You've been invited!")
   end
 end
