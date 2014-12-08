50546db92e847a5b6d769f6f3631ff0508291b41
ceguster <cguster@gmail.com>
2014-12-08 11:09:54 -0500

    jk lol not going to use fog yet

---
+++ config/initializers/fog.rb
@@ -0,0 +1,7 @@
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     provider:           'AWS',
+#     aws_access_key_id:  'xxx',
+#     aws_secret_access_key: 'yyy'
+#   }
+#   
