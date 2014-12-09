ea64b4c3d658fcedb4c60397286481fba73f0cf6
brianneking86 <brianneking86@gmail.com>
2014-12-09 17:30:51 -0500

    updated js some more

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,35 +3,34 @@ $(document).ready(function(){
 
   $('.event-menu').on('click', '.unclaimed', function(e){
     e.preventDefault();
-    var eventId = $(this).parent().data('id');
+    var eventId = $(this).parents('ul').data('id');
     var contributionId = $(this).data('id');
-    var liEl = $(this).parent();
-    
+    var liEl = $(this).siblings('li');
     var username = $(this).parents('ul').attr('id');
     $.ajax ({
       url: '/events/' + eventId + '/contributions/' + contributionId ,
       method: 'patch',
       success: function(){
         $('#unclaimed-' + contributionId).remove();
-        $(liEl).append('<div id="claim-name-' + contributionId + '">claimed by ' + username + ' -</div><div class="claimed" id="claimed-' + contributionId + '">Unclaim</div>');
+        $('.claimed-unclaimed-' + contributionId ).prepend('<div id="claim-name-' + contributionId + '"class="claim-name">claimed by ' + username + ' </div><div class="claimed" id="claimed-' + contributionId + ' data-id="' + contributionId + '">Unclaim</div>');
       }
     });
   });
 
   $('.event-menu').on('click', '.claimed', function(e){
     e.preventDefault();
-    var eventId = $(this).parent().data('id');
+    var eventId = $(this).parents('ul').data('id');
     var contributionId = $(this).data('id');
-    var liEl = $(this).parent();
-    
-    var claimName = $(this).siblings('#claim-name-' + contributionId)
+    var liEl = $(this).siblings('li');
+    var claimName = $(this).siblings('#claim-name-' + contributionId);
+    // debugger;
     $.ajax ({
       url: '/events/' + eventId + '/contributions/' + contributionId ,
       method: 'patch',
       success: function(){
         $('#claimed-' + contributionId).remove();
         $(claimName).remove();
-        $(liEl).append('<div class="unclaimed" id="unclaimed-' + contributionId + '">Claim</div>');
+        $('.claimed-unclaimed-' + contributionId).prepend('<div class="unclaimed" id="unclaimed-' + contributionId + '"data-id="' + contributionId + '">Claim</div>');
       }
     }); 
   });
